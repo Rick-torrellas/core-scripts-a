f4f084716543db025430c29a0b06825f6bc7861c
Ricardo Torrellas <ricardo.torrejas@gmail.com>
2022-03-17 10:48:30 -0400

    feat: nucleo

--- src/services/dirPromise.js
+++ src/services/dirPromise.js
@@ -1,11 +1,71 @@
 const {mkdirSync,rmSync,existsSync} = require("fs");
+const {execSync} = require("child_process");
 const debug = require('./debug');
 //TODO: se puede crear una funcion replaceDir, y ejecutar createDir({dir},{Debug,overwrite=true,recursive=true})
 function checkDir({dir},{Debug}) {
 //TODO: crear una vercion de esta funcion que verifique el contenido de la carpeta checkDirContent()
     const name = "checkDir";
+    debug.name(Debug, name);
+    return new Promise(resolve=>{
+      resolve(existsSync(dir))
+    })
 }
-
+/**
+ * 
+ * @param {string} attr Es el atributo que se asignara a la carpeta, estos pueden ser `h`: Hidden,`r`: Read-only,`s`: System,`a`: Archive,`t`: Temporary,`c`: Compressed,`o`: Offline,`i`: Not indexed,`e`: Encrypted,`x`: No Scrub,`u`: UnPinned,`p`: Pinned,`m`: Recall on Data Access 
+ * @returns 
+ */
+function editAtribute({attr,dir,state},{Debug}) {
+  const name = "editAtribute";
+  debug.name(Debug, name);
+  return new Promise((resolve)=>{
+      resolve(checkDir({dir},{Debug}));
+  })
+  .then(res => {
+    if(res) {
+      const command = `attrib ${state}${attr} ${dir}`; 
+      if (attr !== "h" || attr !== "r" || attr !== "s" || attr !== "a" || attr !== "t" || attr !== "c" || attr !== "o" || attr !== "i" || attr !== "e" || attr !== "x" || attr !== "u" || attr !== "p" || attr !== "m" ) throw new Error(`El atributo: ${attr} no es valido, mirar los atributos validos.`);
+      if (!(state == "-" || state == "+")) throw new Error(`El estado: ${state} no es valido, mirar los estados validos.`);
+      const result = execSync(command);
+      console.log(result.toString());
+      return(true);
+    } else {
+      throw new Error(`No existe el directorio: ${dir}`);
+    }
+  })
+  .catch(err => {
+    debug.error(err);
+  });
+}
+/** @param {{
+  content: Array<string>, 
+ *}}
+ * content: Un array con las rutas de las carpetas que se quieren crear. La carpeta contenedora no tiene que existir anteriormente.
+ */
+async function addContent({content},{Debug}) {
+  try {
+    const NAME_ = "addContent";
+  debug.name(Debug, NAME_);
+  const arg = {
+    content,
+  };
+  debug.values(Debug, arg);
+  for (const key in content) {
+    if (Object.hasOwnProperty.call(content, key)) {
+      const element = content[key];
+      if (await checkDir({dir: element}) == false) {
+       await createDir({dir: element},{Debug,recursive: true});
+      } else {
+        console.log(`${element}:' Ya esta creado'`);
+      }
+    }
+  }
+  debug.done(Debug, NAME_);
+  return true;
+  } catch (error) {
+    debug.error(error);
+  }
+}
 /**
  * Crea una carpeta 
  * @returns True si logro crearla.
@@ -38,4 +98,7 @@ function createDir({dir},{Debug=false,overwrite=false,recursive=false}) {
 }
 module.exports = {
     createDir,
+    editAtribute,
+    addContent,
+    checkDir
 }
